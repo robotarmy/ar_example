c98eb094b65f523f81644e45f0809f9e619824ee
Curtis Schofield <github@robotarmyma.de>
2011-02-08 12:07:25 -0800

    ran bundle && rails g rspec:install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArExample::Application.config.secret_token = 'bae0147e316a1d01054ebfa94704b846520cc60c015bdb8254472dd2b26bbb3edded2a38487dc028e7c303f88bdb9ee530a23ea8d904b479a9c76776e95bcd5e'
